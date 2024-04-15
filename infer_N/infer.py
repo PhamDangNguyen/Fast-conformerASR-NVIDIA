6ff2b4c539fc4278e5410a9dd53cb19e16e3a90a
Dang Nguyen <dangnguyen667201@gmail.com>
2024-04-15 08:42:43 +0700

    done_train_finetune

--- infer_N/infer.py
+++ infer_N/infer.py
@@ -1,7 +1,18 @@
 import nemo.collections.asr as nemo_asr
-
+import torch
 # Load the ASR model checkpoint
-model_ckpt = "a.ckpt"
+model_ckpt = "0.0227_wer.ckpt"
 model = nemo_asr.models.EncDecCTCModelBPE.load_from_checkpoint(model_ckpt)
-
-print(model.transcribe(['0_1ch.wav']))
+if torch.cuda.is_available():
+    # Chuyển model và dữ liệu sang GPU
+    device = torch.device("cuda")
+    model.to(device)
+    print("Model đang sử dụng GPU")
+else:
+    print("CUDA không có sẵn, model sẽ sử dụng CPU")
+#infer batch
+predicted_text = model.transcribe(
+    paths2audio_files=['0_1ch.wav', '2_1ch.wav'],
+    batch_size=2  # batch size to run the inference with
+)
+print(predicted_text)
